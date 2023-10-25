915eea89f553eeaf76e38d31ffd1c77b968890ad
Ryan <ryantwade@hotmail.com>
2023-10-25 15:01:18 -0400

    finished data fetching module

--- app/routes/__app/expenses/$id.jsx
+++ app/routes/__app/expenses/$id.jsx
@@ -1,16 +1,20 @@
 // /expenses/<some-id> => /expenses/expense-1, /expenses/e-1
 
-import { useNavigate } from '@remix-run/react';
+import { redirect } from "@remix-run/node";
+import { useNavigate } from "@remix-run/react";
 
-import ExpenseForm from '~/components/expenses/ExpenseForm';
-import Modal from '~/components/util/Modal';
+import ExpenseForm from "~/components/expenses/ExpenseForm";
+import Modal from "~/components/util/Modal";
+import { deleteExpense, updateExpense } from "~/data/expenses.server";
+import { validateExpenseInput } from "~/data/validation.server";
+// import { getExpense } from '~/data/expenses.server';
 
 export default function UpdateExpensesPage() {
   const navigate = useNavigate();
 
   function closeHandler() {
     // navigate programmatically
-    navigate('..');
+    navigate("..");
   }
 
   return (
@@ -18,4 +22,32 @@ export default function UpdateExpensesPage() {
       <ExpenseForm />
     </Modal>
   );
+}
+
+// export async function loader({params}) {
+//   console.log('EXPENSE ID LOADER');
+//   const expenseId = params.id;
+//   const expense = await getExpense(expenseId)
+//   return expense;
+// }
+
+export async function action({ params, request }) {
+  const expenseId = params.id;
+
+  if (request.method === "PATCH") {
+    const formData = await request.formData();
+    const expenseData = Object.fromEntries(formData);
+
+    try {
+      validateExpenseInput(expenseData);
+    } catch (error) {
+      return error;
+    }
+
+    await updateExpense(expenseId, expenseData);
+    return redirect("/expenses");
+  } else if (request.method === "DELETE") {
+    await deleteExpense(expenseId);
+    return { deletedId: expenseId };
+  }
 }
